63a75d6c67a5e11ef59b697cb1b291bb089dfd7c
Augustine-dev <157492187+Augustine-dev@users.noreply.github.com>
2025-02-17 21:49:49 -0400

    At task 8

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,5 @@
 import click, sys
-from models import db, User
+from models import db, User, Todo
 from app import app
 from sqlalchemy.exc import IntegrityError
 
@@ -10,9 +10,10 @@ def initialize():
   db.init_app(app)
   db.create_all()
   bob = User('bob', 'bob@mail.com', 'bobpass')
-  print(bob)
+  bob.todos.append(Todo('wash car'))
   db.session.add(bob)
   db.session.commit()
+  print(bob)
   print('database intialized')
 
 @app.cli.command("get-user", help="Retrieves a User")
@@ -70,3 +71,12 @@ def delete_user(username):
   db.session.delete(bob)
   db.session.commit()
   print(f'{username} deleted')
+
+@app.cli.command('get-todos')
+@click.argument('username', default='bob')
+def get_user_todos(username):
+  bob = User.query.filter_by(username=username).first()
+  if not bob:
+      print(f'{username} not found!')
+      return
+  print(bob.todos)
